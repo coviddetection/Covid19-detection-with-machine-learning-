3d49e69f629fa0fcff26d892521a57dd23178d59
coviddetection <64379056+coviddetection@users.noreply.github.com>
2020-04-27 14:27:53 +0300

    Add files via upload

---
+++ kurulum update.bat
@@ -0,0 +1,15 @@
+@echo off
+title kurulum 
+start python kurulum.exe 
+set/p devam et:
+pythom -m pip install numpy
+python -m pip install --upgrade tensorflow
+python -m pip install tensor
+pythom -m pip install sklearn
+pythom -m pip install pandas
+pythom -m pip install pillow
+pythom -m pip install keras 
+set/p konum=python kurulum konumu:
+copy cudart64_101.dll %konum%
+echo kurulum tamamlandi
+python -m detection.py 
